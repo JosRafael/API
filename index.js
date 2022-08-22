9c53416a2d2392cb23009f14b52312f054c7f8d6
JRafael <rafaelmatiashue@gmail.com>
2022-08-22 19:57:16 -0300

    config: API REST, Padrão CRUD e Rotas

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 
-app.get('/teste', (req, res) => {
-    console.log('Achou minha rota')//regra de negocio
-    res.json({mensagem: 'Rota Funcionando'})
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+   //PEGAR TODOS OS REGISTROS
+    res.json({mensagem: 'PEGAR TODOS OS REGISTROS'});
+});
+
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({mensagem: `PEGAR SOMETE O REGISTRO COM O ID: ${id}` });
+});
+
+//CRIANDO UM REGISTRO 
+app.post('/', (req, res) => {
+    const body = req.body;
+    res.json({mensagem: 'Criar usuario'});
+});
+
+app.put('/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({mensagem: `ATUALIZAR O REGISTRO SOMENTO COM O ID: ${id}`});
+});
+
+app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({mensagem: `DELETAR SOMENTO COM O ID: ${id}`});
 });
 
 app.listen(4200, () => {
